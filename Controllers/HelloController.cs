ea0f9fe6e43799f77ce628a0371896d635efe343
Diamantine <meteoritus75@gamil.com>
2024-06-18 11:32:13 +0200

    Challenge ASP.NET Routage

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_ROUTAGE.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string SayHello()
+        {
+            return "Hello !";
+        }
+
+        
+        public string YellHello()
+        {
+            return "HELLO !";
+        }
+        [Route("/Say")]
+        public string Say()
+        {
+            return "Hello !";
+        }
+
+        [Route("/Yell")]
+        public string Yell()
+        {
+            return "HELLO !";
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using ASP.NET_ROUTAGE.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NET_ROUTAGE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
